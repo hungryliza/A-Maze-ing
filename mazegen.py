2d0df15a3dfc7ff6328ae65b437a791eaae15a7b
hungryliza <53476586+hungryliza@users.noreply.github.com>
2026-04-21 17:45:29 +0100

    Update mazegen.py
    grid and 42 pattern

--- mazegen.py
+++ mazegen.py
@@ -1,14 +1,35 @@
 import random
 
+class MazeGrid:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.cells = [[15 for col in range(self.width)]for row in range(self.height)]
+
+
 class MazeGenerator:
-    def __init__(self, width, height, entry, exit, perfect, seed, grid):
+    def __init__(self, width, height, entry, exit, perfect=True, seed=0):
         self.width = width
         self.height = height
         self.entry = entry
         self.exit = exit
         self.perfect = perfect
         self.seed = seed
-        self.grid = grid
+        random.seed(self.seed)
+        self.basegrid = MazeGrid(self.height, self.width)
+
+    def generate(self):
+        self.pattern()
+
+    def pattern(self):
+        fortytwo = [(0,0),(0,1),(0,2),(1,2),(2,2),(2,3),(2,4),(4,0),
+                    (5,0),(6,0),(6,1),(4,2),(5,2),(6,2),(4,3),(4,4),(5,4),(6,4)]
+        placed = []
+        placex = int((self.width - 7) / 2)
+        placey = int((self.height - 6) / 2)
+        for (i,j) in fortytwo:
+            placed.append((i + placex, j + placey))
+        return (placed)
 
 
 
@@ -18,5 +39,6 @@ if __name__ == "__main__":
         entry=(0,0), exit=(19,14),
         perfect=True, seed=42
     )
+    gen.pattern(7, 5)
     gen.generate()
-    print(gen.grid)
+    print(gen.basegrid.cells)
